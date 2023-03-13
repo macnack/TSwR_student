f3f372c548a9c260d25130bcf5508d9a081eb38e
Maciej <maciej.krupka@rad4m.com>
2023-03-13 16:16:54 +0100

    Added 3poly trajectory

--- trajectory_generators/poly3.py
+++ trajectory_generators/poly3.py
@@ -11,10 +11,12 @@ class Poly3(TrajectoryGenerator):
         Please implement the formulas for a_0 till a_3 using self.q_0 and self.q_k
         Assume that the velocities at start and end are zero.
         """
-        self.a_0 = None
-        self.a_1 = None
-        self.a_2 = None
-        self.a_3 = None
+        self.q_dot_0 = 0
+        self.q_dot_1 = 0
+        self.a_0 = self.q_0
+        self.a_1 = self.q_dot_0 + 3 * self.q_0
+        self.a_2 = self.q_k 
+        self.a_3 = 3 * self.q_k - self.q_dot_1
 
     def generate(self, t):
         """
@@ -25,6 +27,6 @@ class Poly3(TrajectoryGenerator):
         """
         t /= self.T
         q = self.a_3 * t**3 + self.a_2 * t**2 * (1 - t) + self.a_1 * t * (1 - t)**2 + self.a_0 * (1 - t)**3
-        q_dot = None
-        q_ddot = None
+        q_dot = self.a_3 * 3.0 * t**2 + self.a_2 * (2.0 - 3.0 * t) * t + self.a_1 * ( 3 * t**2 - 4*t + 1 ) - 3 * self.a_0 * ( t - 1)**2
+        q_ddot = self.a_3 * 6.0 * t + self.a_2 * ( 2 - 6*t ) + self.a_1 * ( 6*t - 4 ) + self.a_0 * 6 * (1 -t)
         return q, q_dot / self.T, q_ddot / self.T**2
