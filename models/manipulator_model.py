b3b4965005895b6b7cb3822895ca1a549620a2a9
Maciej <maciej.krupka@rad4m.com>
2023-03-13 16:16:30 +0100

    add 2DoF model

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -15,19 +15,36 @@ class ManiuplatorModel:
         self.m3 = 0.0
         self.r3 = 0.01
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+        self.alpha = self.I_1 + self.I_2 + self.I_3 + \
+                     self.m1 * ( ( self.l1 / 2.0 ) ** 2 ) + \
+                     self.m2 * ( self.l1 ** 2 + ( self.l2 / 2.0 ) ** 2) + \
+                     self.m3 * ( self.l1 ** 2 + self.l2 ** 2)
+        self.beta = self.m2 * self.l1 * ( self.l2 / 2.0 ) + \
+                    self.m3 * self.l1 * self.l2
+        self.gamma = self.I_2 + self.I_3 + \
+                     self.m2 * (( self.l2 / 2.0 ) ** 2) + \
+                     self.m3 * self.l2 ** 2
 
     def M(self, x):
         """
         Please implement the calculation of the mass matrix, according to the model derived in the exercise
         (2DoF planar manipulator with the object at the tip)
         """
-        q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
-
+        _, q2, _, _ = x
+        m11 = self.alpha + 2 * self.beta * np.cos(q2)
+        m12 = self.gamma + self.beta * np.cos(q2)
+        m21 = self.gamma + self.beta * np.cos(q2)
+        m22 = self.gamma
+        return np.array( [ m11, m12, m21, m22] ).reshape(2,2)
+    
     def C(self, x):
         """
         Please implement the calculation of the Coriolis and centrifugal forces matrix, according to the model derived
         in the exercise (2DoF planar manipulator with the object at the tip)
         """
-        q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+        _, q2, q1_dot, q2_dot = x
+        c11 = - self.beta * np.sin(q2) * q2_dot
+        c12 = - self.beta * np.sin(q2) * (q1_dot + q2_dot)
+        c21 = self.beta * np.sin(q2) * q1_dot
+        c22 = 0
+        return np.array( [c11, c12, c21, c22] ).reshape(2,2)
