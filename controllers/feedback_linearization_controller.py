0a4a19a35c60f1107325db68e37b4ad7074e6275
Maciej <maciej.krupka@rad4m.com>
2023-03-17 14:19:43 +0100

    Fix: feedback controler

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -6,31 +6,24 @@ from .controller import Controller
 class FeedbackLinearizationController(Controller):
     def __init__(self, Tp):
         self.model = ManiuplatorModel(Tp)
-        self.Kd = np.array([10.0, 0]).reshape(1,2)
-        self.Kp = np.array([10.0, 0]).reshape(1,2)
+        self.Kd = np.array([100.0, 100.0]).reshape(1,2)
+        self.Kp = np.array([100.0, 100.0]).reshape(1,2)
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         """
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        q, q_dot = np.hsplit(x[np.newaxis,:], 2)
-        q = np.transpose(q)
-        q_dot = np.transpose(q_dot)
+        q = np.transpose(np.array([x[:2]]))
+        q_dot = np.transpose(np.array([x[2:]]))
         if q_r_ddot.shape != (2,1):
             v = q_r_ddot[:, np.newaxis]
         else:
             v = q_r_ddot
         # Adding feedback
-        v += self.Kd@( q_dot - q_r_dot[:, np.newaxis] ) + self.Kp @ (q - q_r[:, np.newaxis])
+        v -= self.Kd@( q_dot - q_r_dot[:, np.newaxis] ) + self.Kp @ (q - q_r[:, np.newaxis])
 
         M = self.model.M(x)
         C = self.model.C(x)
         tau = M @ v + C @ q_dot
-        if False:
-            print("tau:={shape}".format(shape=tau.shape))
-            print("M:={shape}".format(shape=M.shape))
-            print("v:={shape}".format(shape=v.shape))
-            print("C:={shape}".format(shape=C.shape))
-            print("q_dot:={shape}".format(shape=q_dot.shape))
         return tau
